b8ba08fda5aec392651dc413c5fbc781e80da25c
zhengzhang <zhengzhang@ruijie.com.cn>
2019-03-08 17:22:45 +0800

    redis lock

---
+++ spring-boot-study-redis-lock/src/main/java/com/HelloController.java
@@ -0,0 +1,33 @@
+package com;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@Autowired
+	DistributedLockHandler distributedLockHandler;
+
+    @RequestMapping("/test")
+    private String hello() {
+    	System.out.println("start service 1");
+    	Lock lock=new Lock("redis-lock","my-redis-lock");
+		if(distributedLockHandler.tryLock(lock)){
+			System.out.println("start service 2");
+			try{
+				Thread.sleep(10000);
+			}catch(Exception e){
+				System.out.println(e);
+			}
+			
+		    distributedLockHandler.releaseLock(lock);
+		    System.out.println("end service");
+		}
+        return "Hello World!";
+    }
+
+
+}
