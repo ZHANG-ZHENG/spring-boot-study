6c55df0b5f05396a28e9cc59fb83cb07692bda45
zhengzhang <zhengzhang@ruijie.com.cn>
2019-04-10 15:11:22 +0800

    es but fail

---
+++ spring-boot-study-elasticsearch/src/main/java/com/demo/CommodityRepository.java
@@ -0,0 +1,9 @@
+package com.demo;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommodityRepository extends ElasticsearchRepository<Commodity, String> {
+
+}
---
+++ spring-boot-study-elasticsearch/src/test/java/com/ElasticsearchExampleApplicationTests.java
@@ -0,0 +1,88 @@
+package com;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.demo.Commodity;
+import com.demo.CommodityService;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElasticsearchExampleApplicationTests {
+
+    @Autowired
+    private CommodityService commodityService;
+
+    @Test
+    public void contextLoads() {
+        System.out.println(commodityService.count());
+    }
+
+//    @Test
+//    public void testInsert() {
+//        Commodity commodity = new Commodity();
+//        commodity.setSkuId("1501009001");
+//        commodity.setName("原味切片面包（10片装）");
+//        commodity.setCategory("101");
+//        commodity.setPrice(880);
+//        commodity.setBrand("良品铺子");
+//        commodityService.save(commodity);
+//
+//        commodity = new Commodity();
+//        commodity.setSkuId("1501009002");
+//        commodity.setName("原味切片面包（6片装）");
+//        commodity.setCategory("101");
+//        commodity.setPrice(680);
+//        commodity.setBrand("良品铺子");
+//        commodityService.save(commodity);
+//
+//        commodity = new Commodity();
+//        commodity.setSkuId("1501009004");
+//        commodity.setName("元气吐司850g");
+//        commodity.setCategory("101");
+//        commodity.setPrice(120);
+//        commodity.setBrand("百草味");
+//        commodityService.save(commodity);
+//
+//    }
+//
+//    @Test
+//    public void testDelete() {
+//        Commodity commodity = new Commodity();
+//        commodity.setSkuId("1501009002");
+//        commodityService.delete(commodity);
+//    }
+//
+//    @Test
+//    public void testGetAll() {
+//        Iterable<Commodity> iterable = commodityService.getAll();
+//        //iterable.forEach(e->System.out.println(e.toString()));
+//		iterable.forEach(new Consumer<Commodity>() {
+//			@Override
+//			public void accept(Commodity t) {
+//				System.out.println(t.toString());
+//			}
+//		});
+//    }
+//
+//    @Test
+//    public void testGetByName() {
+//        List<Commodity> list = commodityService.getByName("面包");
+//        System.out.println(list);
+//    }
+//
+//    @Test
+//    public void testPage() {
+//        Page<Commodity> page = commodityService.pageQuery(0, 10, "切片");
+//        System.out.println(page.getTotalPages());
+//        System.out.println(page.getNumber());
+//        System.out.println(page.getContent());
+//    }
+}
