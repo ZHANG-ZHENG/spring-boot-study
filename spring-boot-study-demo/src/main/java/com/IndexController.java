63a376b1ff3e0346679477253558cad0c556d8f5
zhengzhang <35785077@qq.com>
2025-04-10 15:21:37 +0800

    query

--- spring-boot-study-demo/src/main/java/com/IndexController.java
+++ spring-boot-study-demo/src/main/java/com/IndexController.java
@@ -1,7 +1,13 @@
 package com;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @RestController
 @RequestMapping("/demo")
@@ -9,8 +15,25 @@ public class IndexController {
 
     // http://localhost:8080/demo/test
     @RequestMapping(value = "/test")
-    public String index() {
+    public Map<String, Object> test(String query) {
+        SimpleDateFormat sdf = new SimpleDateFormat("MMddHHmmss");
+        Date now = new Date();
 
-        return "Hello World demo!!!";
+        Map<String, Object> result = new HashMap<>();
+        result.put("query", query);
+        result.put("result", sdf.format(now));
+        return result;
+    }
+
+    // http://localhost:8080/demo/query/baidu.com
+    @GetMapping("/query/{query}")
+    public Map<String, Object> test2(@PathVariable String query) {
+        SimpleDateFormat sdf = new SimpleDateFormat("MMddHHmmss");
+        Date now = new Date();
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("query", query);
+        result.put("result", sdf.format(now));
+        return result;
     }
 }
